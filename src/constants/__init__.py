b3f2ac125a65a2cf3a12153b2c68ac56e66a6f3a
Himanshu6371 <157985138+Himanshu6371@users.noreply.github.com>
2025-03-24 16:45:26 +0530

    completed

---
+++ src/constants/__init__.py
@@ -0,0 +1,6 @@
+MONGODB_URL_KEY: str = "mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
